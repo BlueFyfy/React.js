3040ff105cca7d9e8b903b471233fa04c11a9e7b
BlueFyfy <71499179+BlueFyfy@users.noreply.github.com>
2022-10-22 18:42:30 +0800

    Add files via upload

---
+++ react-props-practice/src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imgURL:
+      "https://tse3-mm.cn.bing.net/th/id/OIP-C.gTxpsdpCMs-UOwH_2-NZZAHaEo?pid=ImgDet&rs=1",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "G.M.E",
+    imgURL:
+      "https://tse1-mm.cn.bing.net/th/id/OIP-C.xKeyw_FvYRhD0ul7l82rLQHaHa?pid=ImgDet&rs=1",
+    phone: "+987 654 321",
+    email: "gemdeng@nowhere.com"
+  },
+  {
+    name: "Taylor Swift",
+    imgURL:
+      "https://tse1-mm.cn.bing.net/th/id/OIP-C.dOCQFnnJyxaGKR2A0BaswwHaNK?pid=ImgDet&rs=1",
+    phone: "+918 372 574",
+    email: "gmail@taylorswift.com"
+  }
+];
+
+export default contacts;
