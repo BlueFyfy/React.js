eeb48d84230724ce3b34798acf83125fa3033b98
BlueFyfy <71499179+BlueFyfy@users.noreply.github.com>
2022-10-22 18:48:45 +0800

    Add files via upload

---
+++ keeper-app-part-1/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <html>
+      <body>
+        <Header />
+        <Footer />
+        <Note />
+      </body>
+    </html>
+  );
+}
+
+export default App;
